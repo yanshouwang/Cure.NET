6dcc012ebbfadddace54c1f2d3b6d0f71e6657a2
yanshouwang <yanshouwang@outlook.com>
2020-08-27 17:26:02 +0800

    添加 GeometryAnimation

---
+++ AOtter.WPF/Media/Animation/GeometryAnimation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOtter.WPF.Media.Animation
+{
+    class GeometryAnimation
+    {
+    }
+}
---
+++ AOtter.WPF/Media/Animation/GeometryAnimationBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace AOtter.WPF.Media.Animation
+{
+    public abstract class GeometryAnimationBase : AnimationTimeline
+    {
+        #region 构造函数
+        protected GeometryAnimationBase()
+            : base()
+        {
+
+        }
+        #endregion
+
+        #region Freezable
+        public new GeometryAnimationBase Clone()
+        {
+            return (GeometryAnimationBase)base.Clone();
+        }
+        #endregion
+
+        #region IAnimation
+        public override sealed Type TargetPropertyType
+        {
+            get
+            {
+                ReadPreamble();
+                return typeof(Geometry);
+            }
+        }
+
+        public override object GetCurrentValue(object defaultOriginValue, object defaultDestinationValue, AnimationClock animationClock)
+        {
+            return GetCurrentValue((Geometry)defaultOriginValue, (Geometry)defaultDestinationValue, animationClock);
+        }
+
+        public Geometry GetCurrentValue(Geometry defaultOriginValue, Geometry defaultDestinationValue, AnimationClock animationClock)
+        {
+            ReadPreamble();
+            if (animationClock == null)
+            {
+                throw new ArgumentNullException("animationClock");
+            }
+            if (animationClock.CurrentState == ClockState.Stopped)
+            {
+                return defaultDestinationValue;
+            }
+            return GetCurrentValueCore(defaultOriginValue, defaultDestinationValue, animationClock);
+        }
+
+        protected abstract Geometry GetCurrentValueCore(Geometry defaultOriginValue, Geometry defaultDestinationValue, AnimationClock animationClock);
+        #endregion
+    }
+}
