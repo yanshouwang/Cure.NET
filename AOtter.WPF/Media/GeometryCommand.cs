80f8ac5e20043591209c997b4da0287309469639
yanshouwang <yanshouwang@outlook.com>
2020-08-29 17:34:04 +0800

    GeometryAnimation 部分实现

---
+++ AOtter.WPF/Media/GeometryCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace AOtter.WPF.Media
+{
+    public class GeometryCommand
+    {
+        public string Command { get; }
+        public IList<Point> Args { get; }
+
+        public GeometryCommand(string command, IList<Point> args)
+        {
+            Command = command;
+            Args = args;
+        }
+
+        public override string ToString()
+        {
+            var args = Args.Aggregate(string.Empty, (total, next) => $"{total} {next}");
+            return $"{Command}{args}";
+        }
+    }
+}
---
+++ AOtter.WPF/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AOtter.WPF
+{
+    static class StringExtensions
+    {
+        public static T GetValue<T>(this string str)
+        {
+            var type = typeof(T);
+            var value = (T)TypeDescriptor.GetConverter(type).ConvertFromString(str);
+            return value;
+        }
+    }
+}
