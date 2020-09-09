958c49ac966e175f38a5aa93efc5aa5f7809d60f
yanshouwang <yanshouwang@outlook.com>
2020-09-09 11:09:24 +0800

    添加 ShapeExtension

---
+++ Cure.WPF/Shapes/ShapeExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Cure.WPF.Shapes
+{
+    static class ShapeExtension
+    {
+        public static double GetStrokeThickness(this Shape shape)
+        {
+            var type = shape.GetType();
+            var method = type.GetMethod("GetStrokeThickness", BindingFlags.Instance | BindingFlags.NonPublic);
+            var obj = (double)method.Invoke(shape, null);
+            return obj;
+        }
+
+        public static Pen GetPen(this Shape shape)
+        {
+            var type = shape.GetType();
+            var method = type.GetMethod("GetPen", BindingFlags.Instance | BindingFlags.NonPublic);
+            var obj = (Pen)method.Invoke(shape, null);
+            return obj;
+        }
+    }
+}
